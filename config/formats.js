b466372bae7f10acf3bfa09ec47e92ab378021a7
jackofspadesman <jackofspadesman@gmail.com>
2017-06-17 09:33:11 -0400

    Unbanned Necrozma from Alolan VGC 2016

--- config/formats.js
+++ config/formats.js
@@ -298,7 +298,7 @@ exports.Formats = [
 	  ruleset: ['Pokemon', 'Species Clause', 'Nickname Clause', 'Item Clause', 'Team Preview', 'Cancel Mod'],
 	  banlist: [
 	    'Illegal', 'Unreleased', 'Mew', 'Celebi', 'Jirachi', 'Deoxys', 'Deoxys-Attack', 'Deoxys-Defense', 'Deoxys-Speed', 'Phione', 'Manaphy', 'Darkrai',
-	    'Shaymin', 'Shaymin-Sky', 'Arceus', 'Victini', 'Keldeo', 'Meloetta', 'Genesect', 'Diancie', 'Hoopa', 'Hoopa-Unbound', 'Volcanion', 'Necrozma', 'Magearna', 'Marshadow',
+	    'Shaymin', 'Shaymin-Sky', 'Arceus', 'Victini', 'Keldeo', 'Meloetta', 'Genesect', 'Diancie', 'Hoopa', 'Hoopa-Unbound', 'Volcanion', 'Magearna', 'Marshadow',
 	  ],
 	  requirePentagon: true,
 	  onValidateTeam: function (team) {
